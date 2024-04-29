c3e4167d6c9346f7159a6b4ac3da0a2131b0e575
Xeless <tomdelintepro@gmail.com>
2024-04-29 14:32:29 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
-import { addTask } from './task.js'; // Importez la fonction addTask depuis le fichier task.js
+import { addTask } from './task.js';
 
 const taskForm = document.getElementById("form");
 const todoInput = document.getElementById("todo");
 
 
 taskForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Empêche le formulaire de se soumettre et de recharger la page
-    const taskContent = todoInput.value.trim(); // Récupère la valeur de l'input en supprimant les espaces vides au début et à la fin
+    event.preventDefault(); 
+    const taskContent = todoInput.value.trim(); 
     if (taskContent !== "") {
-        addTask(taskContent); // Appelle la fonction addTask pour ajouter la tâche à la liste
-        todoInput.value = ""; // Efface le contenu de l'input après l'ajout de la tâche
+        addTask(taskContent); 
+        todoInput.value = ""; 
     }
 });
--- assets/js/task.js
+++ assets/js/task.js
@@ -1,30 +1,32 @@
-const tasks = []
-
-
-
-
-
+const tasks = [];
+const listDiv = document.querySelector(".list-item");
+const ul = document.createElement("ul");
 
 export function addTask(taskContent) {
-    const listDiv = document.querySelector(".list-item");
-    const ul = document.createElement("ul");
 
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox"; // Définit le type de l'input comme une case à cocher
-    const label = document.createElement("label");
-    label.textContent = taskContent;
-
-    const iconRemove = document.createElement("i");
-    iconRemove.classList.add("fa-solid", "fa-eraser");
-    iconRemove.addEventListener("click", function() {
-        ul.removeChild(li); // Supprime l'élément <li> de la liste <ul>
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox"; 
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      li.classList.add("completed");
+    }
+  });
+  const label = document.createElement("label");
+  label.textContent = taskContent;
+  const iconRemove = document.createElement("i");
+  iconRemove.classList.add("fa-solid", "fa-eraser");
+  iconRemove.addEventListener("click", function () {
+    ul.removeChild(li); 
+    const test = document.querySelectorAll("li.completed");
+    test.forEach((item) => {
+      item.remove();
     });
+  });
 
-    li.appendChild(checkbox); // Ajoute la case à cocher à l'élément <li>
- li.appendChild(label); // Ajoute le texte de la tâche à l'élément <li>
-    li.appendChild(iconRemove); // Ajoute l'icône de suppression à l'élément <li>
-    ul.appendChild(li); // Ajoute l'élément <li> à la liste <ul>
-
-    listDiv.appendChild(ul); // Ajoute la liste <ul> à la liste
+  li.appendChild(checkbox); 
+  li.appendChild(label);
+  li.appendChild(iconRemove); 
+  ul.appendChild(li); 
+  listDiv.appendChild(ul); 
 }
